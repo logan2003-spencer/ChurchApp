635b7ccb8d19db182a3795488e987df70f01cf02
brynnesplin <103144915+brynnesplin@users.noreply.github.com>
2025-03-14 17:37:33 -0600

    Initial database setup

---
+++ backend/ChurchApp-1/ChurchApp-1/Models/Organization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChurchApp_1.Models;
+
+public partial class Organization
+{
+    public int OrganizationId { get; set; }
+
+    public string OrganizationName { get; set; } = null!;
+
+    public int UnitId { get; set; }
+
+    public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+
+    public virtual Unit Unit { get; set; } = null!;
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+
+    public virtual ICollection<User> UsersNavigation { get; set; } = new List<User>();
+}
