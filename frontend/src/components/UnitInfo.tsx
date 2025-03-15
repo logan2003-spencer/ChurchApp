0f70bc817229414a3500d01c1c1bab8ef925dcd5
brynnesplin <103144915+brynnesplin@users.noreply.github.com>
2025-03-14 18:54:46 -0600

    Added initial nav to actionbutton

--- frontend/src/components/UnitInfo.tsx
+++ frontend/src/components/UnitInfo.tsx
@@ -8,10 +8,10 @@ export const UnitInfo: React.FC = () => {
       <h1 className={styles.unitName}>Unit Name</h1>
       <h2 className={styles.stakeName}>Stake Name</h2>
       <nav>
-        <ActionButton label="Events" className={styles.btnText} />
-        <ActionButton label="Attendance" className={styles.btnText} />
-        <ActionButton label="Add Event" className={styles.btnText} />
-        <ActionButton label="Ward/Stake Login" className={styles.btnText} />
+        <ActionButton label="Events" className={styles.btnText} to={''} />
+        <ActionButton label="Attendance" className={styles.btnText} to={''}/>
+        <ActionButton label="Add Event" className={styles.btnText} to={''}/>
+        <ActionButton label="Ward/Stake Login" className={styles.btnText} to={''} />
       </nav>
     </section>
   );
